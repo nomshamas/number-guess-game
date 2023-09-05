a4c8afea5aeb47a492e7d87f74aa3542ad4b77a8
nomshamas <nomshamas@gmail.com>
2023-09-05 20:09:51 +0500

    chore:fix-bugs

--- js/script.js
+++ js/script.js
@@ -1,37 +1,54 @@
 // "use strict";
 
-var randomValue = document.getElementById('q-box');
-
-var guessingValue = document.getElementById('guessing');
-
-var scoreValue = document.getElementById('score');
-var hightScoreValue = document.getElementById('h-score');
-
-var inputValue = document.getElementById('input-field').value;
-
-var checkBtn = document.getElementById('check-btn');
-var resetBtn = document.getElementById('reset-btn');
+var randomValue = document.getElementById("q-box");
+var guessingValue = document.getElementById("guessing");
+var scoreValue = document.getElementById("score");
+var hightScoreValue = document.getElementById("h-score");
+var inputValue = document.getElementById("input-field");
+var checkBtn = document.getElementById("check-btn");
+var resetBtn = document.getElementById("reset-btn");
 
 var number = 0;
 number = Math.floor(Math.random() * 20) + 1;
 
-alert(number)
+var currentScore = 20;
 
+randomValue.textContent = number;
+inputValue.focus();
 
-checkBtn.onclick = function() {
-	if(inputValue === number) {
-		randomValue.innerHTML = number;
-		hightScoreValue.textContent = scoreValue
-	 }
-	// else {
-	// 	let currentScore = parseInt(scoreValue.textContent)
-	// 	currentScore--
-	// 	scoreValue.textContent = currentScore;
-	// 	inputValue = '';
-	// }
-	// document.write(randomValue, guessingValue, scoreValue, hightScoreValue, inputValue);
-}
+checkBtn.addEventListener("click", function () {
+  if (inputValue.value < 1 || inputValue.value > 20) {
+    guessingValue.textContent = "Invalid Value!";
+  } else {
+    if (inputValue.value > number && currentScore > 1) {
+      guessingValue.textContent = "Too, High value!";
+      currentScore--;
+      scoreValue.textContent = currentScore;
+    } else if (inputValue.value < number && currentScore > 1) {
+      guessingValue.textContent = "Too, Low value!";
+      currentScore--;
+      scoreValue.textContent = currentScore;
+    } else if(inputValue.value == number && currentScore > 1) {
+      guessingValue.textContent = 'You Won!'
+      guessingValue.style.color = 'green'
+      inputValue.setAttribute('disabled', 'disabled')
+      checkBtn.setAttribute('disabled', 'disabled')
+    }
+  }
+  inputValue.value = "";
+});
 
-resetBtn.onclick = function() {
-	inputValue = '';
-}
+resetBtn.addEventListener("click", function () {
+  randomValue.textContent = "?";
+  guessingValue.textContent = "Start Guessing...";
+  scoreValue.textContent = "20";
+  inputValue.removeAttribute('disabled')
+  checkBtn.removeAttribute('disabled')
+});
+
+inputValue.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    checkBtn.click();
+  }
+});
